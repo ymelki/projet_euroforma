0d74cfab9472f39b8719bf771d36bfa8a5baa443
Yoel Melki <ymelki@gmail.com>
2022-05-18 15:10:42 +0200

    test

---
+++ page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // chaine
+    $mavariable="une chaine de caractere";
+
+    // nombre
+    $mavariable2=12;
+
+    //tab
+    $mavariable3=[12,43,34];
+    var_dump($mavariable3);
+
+
+
+       // afficher les nombre de 0 à 100
+       for ($i=0; $i< 100;$i++) {
+           if ($i==5) {
+               echo "je suis tunisien !";
+           }
+            // afficher du texte
+            echo " ".$i;
+       }
+ 
+    ?>
+
+
+</body>
+</html>
